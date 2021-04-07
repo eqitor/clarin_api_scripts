057b7858eeb85abaf845fe32d15047ab173b4b2d
KornelBl <241391@student.pwr.edu.pl>
2021-04-07 19:25:35 +0200

    cleaning the test.py, time to make pytest file for this purpose

--- src/clarinAPI/TagerAPI.py
+++ src/clarinAPI/TagerAPI.py
@@ -3,11 +3,14 @@ from requests import Response
 
 
 class FileTask:
-    def __init__(self, file: str, options: str = 'any2txt|wcrft2({"guesser":false, "morfeusz2":true})'):
+    def __init__(self, file_path: str, options: str = 'any2txt|wcrft2({"guesser":false, "morfeusz2":true})'):
         self._progress: float = 0
         self._download_url = ""
         self._api = FileTagerAPI()
-        self._task_id = self._api.start_processing(file, options).text
+        self._remote_filepath = self._api.upload_zip_file(file_path)
+        self._options = f"filezip({self._remote_filepath})|"+options+"|dir|makezip"
+        print(self._options)
+        self._task_id = self._api.start_processing(self._options).text
 
     def is_ready(self):
         resp = self._api.get_task_status(self._task_id)
@@ -23,8 +26,10 @@ class FileTask:
     def get_progress(self) -> float:
         return self._progress
 
-    def download_file(self):
-        return self._api.download_task_result(self._download_url)
+    def download_and_save_file(self, out_file):
+        with open(out_file, "wb") as f:
+            a = self._api.download_task_result(self._download_url)
+            f.writelines(a.iter_content())
 
 
 
@@ -54,6 +59,15 @@ class Task:
 class FileTagerAPI:
     """Class providing simple connection to nlprest2 API."""
 
+
+    def upload_zip_file(self,file_path):
+        files = {'file': open(file_path, 'rb')}
+        values = {"Content-Disposition": "form-data; name=\"file\"; filename=\"korpus.zip\"",
+                  "Content-Type": "application/x-zip-compressed"}
+
+        resp = requests.post("http://ws.clarin-pl.eu/nlprest2/base/upload/", files=files, data=values)
+        return resp.text
+
     def send_GET(self, url, task):
         """Sends GET request for given URL and task."""
         if type(task) is requests.models.Response:
@@ -61,11 +75,11 @@ class FileTagerAPI:
 
         full_url = url + task
         print(full_url)
-        request_response = requests.get(full_url)
+        request_response = requests.get(full_url, stream=True)
 
         return request_response
 
-    def start_processing(self, file, options ='any2txt|wcrft2({"guesser":false, "morfeusz2":true})') -> Response:
+    def start_processing(self, options ='any2txt|wcrft2({"guesser":false, "morfeusz2":true})') -> Response:
         """Uploads file and starts processing. Returns Response object."""
         url = r"http://ws.clarin-pl.eu/nlprest2/base/startTask"
 
--- test.py
+++ test.py
@@ -1,52 +1,9 @@
-from src.clarinAPI.processing import CorpusProcessing
-import requests
 from src.clarinAPI.TagerAPI import FileTask
 from time import sleep
 
-text = """Woda jest jedną z najpospolitszych substancji we Wszechświecie.
-Cząsteczka wody jest trzecią najbardziej rozpowszechnioną molekułą w ośrodku międzygwiazdowym, po cząsteczkowym wodorze i tlenku węgla. Jest również szeroko rozpowszechniona w Układzie Słonecznym: stanowi istotny element budowy Ceres i księżyców lodowych krążących wokół planet-olbrzymów, jako domieszka występuje w ich atmosferach, a przypuszcza się, że duże jej ilości znajdują się we wnętrzach tych planet. Jako lód występuje także na części planetoid, a zapewne również na obiektach transneptunowych. Woda jest bardzo rozpowszechniona także na powierzchni Ziemi. Występuje głównie w oceanach, które pokrywają 70,8% powierzchni globu, ale także w rzekach, jeziorach i w postaci stałej w lodowcach. Część wody znajduje się w atmosferze (chmury, para wodna). Niektóre związki chemiczne zawierają cząsteczki wody w swojej budowie (hydraty – określa się ją wówczas mianem wody krystalizacyjnej). Zawartość wody włączonej w strukturę minerałów w płaszczu Ziemi może przekraczać łączną zawartość wody w oceanach i innych zbiornikach powierzchniowych nawet dziesięciokrotnie. 
-Woda występująca w przyrodzie jest roztworem soli i gazów. Najwięcej soli mineralnych zawiera woda morska i wody mineralne; najmniej woda z opadów atmosferycznych. Wodę o małej zawartości składników mineralnych nazywamy wodą miękką, natomiast zawierającą znaczne ilości soli wapnia i magnezu – wodą twardą. Oprócz tego wody naturalne zawierają rozpuszczone substancje pochodzenia organicznego, np. mocznik, kwasy humusowe itp."""
-text2 = """Dodatni wynik na obecność przeciwciał przeciw SARS-CoV-2 w klasie IgG jest tylko potwierdzeniem kontaktu, a niekoniecznie przebycia choroby – stwierdził lekarz Bartosz Fiałek w rozmowie z portalem Medonet.
-
-W jego ocenie testy na pewno będą chętnie kupowane, ale ich walor jest przede wszystkim dydaktyczny. Mogą też zadziałać uspokajająco w razie uzyskania dodatniego wyniku w klasie IgG.
-
-Wynik testu nie pozwoli zdobyć statusu ozdrowieńca, nie pozwoli też lecieć bez kwarantanny do innych krajów."""
-#option = 'any2txt|wcrft2({"guesser":false, "morfeusz2":true})'
-#option = 'any2txt|wcrft2|liner2({"model":"n82"})'
-
-# options = ['any2txt|wcrft2|liner2({"model":"n82"})', 'any2txt|wcrft2({"guesser":false, "morfeusz2":true})']
-# documents = [text, text2]
-# process = CorpusProcessing(documents, options)
-# print(process.process_corpus())
-
-
-files = {'file': open('jajca.zip', 'rb')}
-values = {"Content-Disposition": "form-data; name=\"file\"; filename=\"jajca.zip\"",
-                      "Content-Type": "application/x-zip-compressed"}
-
-resp = requests.post("http://ws.clarin-pl.eu/nlprest2/base/upload/", files=files, data=values)
-
-# resp = requests.post("http://ws.clarin-pl.eu/nlprest2/base/upload/", doc,
-#                      {"Content-Disposition": "form-data; name=\"file\"; filename=\"jajca.zip\"",
-#                       "Content-Type": "application/x-zip-compressed"})
-print(resp)
-print(resp.text)
-
-
-
-task = FileTask(resp.text,
-                f"filezip({resp.text})|any2txt|"
-                "wcrft2({\"guesser\":false, \"morfeusz2\":true})|dir|makezip")
-                # f"filezip({resp.text})"+'|any2txt|wcrft2({' + f"{q}" '"guesser' +
-                # f"{q}" '":false, ' + f"{q}" '"morfeusz2' + f"{q}" '":true})|dir|makezip')
-                #'any2txt|wcrft2({"guesser":false, "morfeusz2":true})')
+task = FileTask("jajca.zip")
 while not task.is_ready():
     print(task.get_progress())
     sleep(0.1)
-result = task.download_file()
-if result.status_code != 200:
-    raise ConnectionError(f"Document was not processed correctly: {result.text}")
-print(result.text)
-
-
+task.download_and_save_file(out_file="resultat.zip")
 
