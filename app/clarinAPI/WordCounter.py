7b04c4a1eee10d4428b198ecc095b8bd6c99cde0
eqitor <pawel.pogudz97@gmail.com>
2021-04-27 11:01:18 +0200

    WordCounter added

---
+++ app/clarinAPI/WordCounter.py
@@ -0,0 +1,13 @@
+class WordCounter:
+
+    def count_words(self, dictionary_data):
+        """Counts words (base words) in given list of dictionaries obtained with Converter
+        :param dictionary_data: dictionary data from Converter
+        :returns counter_dictionary: dictionary with counted words ('word' : number of occurrences)"""
+        counter_dictionary = {}
+        for single_dictionary in dictionary_data:
+            try:
+                counter_dictionary[single_dictionary['base']] += 1
+            except KeyError:
+                counter_dictionary[single_dictionary['base']] = 1
+        return counter_dictionary
