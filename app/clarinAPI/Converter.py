14d8d53cd1ed38f6e3b49e76148ab96582da92dd
eqitor <pawel.pogudz97@gmail.com>
2021-05-13 23:23:55 +0200

    Converter is now allowing user to specify custom ctag filtering, allowed and default ctags are defined in Converter class atributes

--- app/clarinAPI/Converter.py
+++ app/clarinAPI/Converter.py
@@ -4,9 +4,18 @@ import json
 
 class Converter:
 
-    def convert(self, filepath):
-        """Converts XML processing results to list of dictionaries.
+    default_ctags = [
+        'subst', 'adj', 'adv'
+    ]
+
+    allowed_ctags = [
+        'subst', 'adj', 'adv'
+    ]
+
+    def convert(self, filepath, ctag_list):
+        """Converts XML processing results to list of dictionaries with ctag filtering.
         :param filepath: path to XML data
+        :param ctag_list: list of ctags allowed in output list. If None, function returns subbst, adj and adv ctags.
         :returns ortho_list: list of dictionaries"""
 
         mydoc = minidom.parse(filepath)
@@ -16,39 +25,43 @@ class Converter:
 
         ortho_list = []
         for tok, base, ctag in zip(toks, bases, ctags):
-            if self.is_ctag_correct(ctag.firstChild.data):
+            if allowed_tag := self.is_ctag_correct(ctag.firstChild.data, ctag_list):
                 new_dictionary = {
                     'orth': tok.firstChild.data,
                     'base': base.firstChild.data,
-                    'ctag': ctag.firstChild.data,
+                    'ctag': allowed_tag,
                 }
                 ortho_list.append(new_dictionary)
 
         return ortho_list
 
-    def is_ctag_correct(self, ctag):
-        """Checks if given ctag string contains any of fixed tags (noun, adjevtive or adverb)
+    def is_ctag_correct(self, ctag, ctag_list):
+        """Checks if given ctag string contains any of tags specified in ctag_filter list
         :param ctag: ctag string
-        :returns : True if ctag contains any correct tag
+        :param ctag_list: list of ctags allowed in output. If None, function returns subbst, adj and adv ctags.
+        :returns : allowed tag if ctag contains any correct tag, else returns False
         """
         splitted_ctag = ctag.split(":")
-        fixed_tags = [
-            "subst",
-            "adj",
-            "adv",
-        ]
-
         for tag in splitted_ctag:
-            if tag in fixed_tags:
-                return True
+            if tag in ctag_list:
+                return tag
         return False
 
-    def to_json(self, filepath):
-        """Converts XML processing data to JSON
-        :param filepath: path to XML data
-        :returns json_entity: data in JSON format"""
+    def validate_ctag_list(self, ctag_list):
+        """Checks if given ctag list contains unavailable ctags
+        :param ctag: list of ctags"""
+        for ctag in ctag_list:
+            if ctag not in Converter.allowed_ctags:
+                raise Exception(f"{ctag} is not on allowed_ctags list.")
 
-        converted_xml = self.convert(filepath)
+
+    def to_json(self, filepath, ctag_list=default_ctags):
+        """Converts XML processing data to JSON with ctag filtering.
+        :param filepath: path to XML data
+        :param ctag_list: list of ctags allowed in JSON. If None, function returns subbst, adj and adv ctags.
+        :returns json_entity: data in JSON format"""
+        self.validate_ctag_list(ctag_list)
+        converted_xml = self.convert(filepath, ctag_list)
         json_entity = json.dumps(converted_xml)
 
         return json_entity
