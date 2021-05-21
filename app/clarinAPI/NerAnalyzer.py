662dabb9f40c1839d638f84d5e1df6a0c0c8a98d
eqitor <pawel.pogudz97@gmail.com>
2021-05-21 14:41:06 +0200

    NerAnalyzer upgraded.

--- app/clarinAPI/NerAnalyzer.py
+++ app/clarinAPI/NerAnalyzer.py
@@ -1,5 +1,6 @@
 from xml.dom import minidom
 
+
 class NerAnalyzer:
 
     def find_names(self, filepath):
@@ -14,11 +15,25 @@ class NerAnalyzer:
         for tok in toks:
             orth = tok.getElementsByTagName('orth')
             anns = tok.getElementsByTagName('ann')
+            bases = tok.getElementsByTagName('base')
+            ctags = tok.getElementsByTagName('ctag')
             if anns:
                 if anns[0].firstChild.data == '1':
-                    names_list.append({
-                        'orth': orth[0].firstChild.data,
-                        'chan': anns[0].getAttribute('chan'),
-                        'head': anns[0].getAttribute('head')
-                    })
+                    unique = True
+                else:
+                    unique = False
+                names_list.append({
+                    'word': orth[0].firstChild.data,
+                    'original': bases[0].firstChild.data,
+                    'category': anns[0].getAttribute('chan'),
+                    'unique': unique,
+                    'speech': self.create_speech_list(ctags[0].firstChild.data)
+                })
         return names_list
+
+    def create_speech_list(self, ctag):
+        """Takes ctag and returns splitted list with types
+        :param ctag: given ctag
+        :returns splitted_list: list of ctag types"""
+        splitted_list = ctag.split(':')
+        return splitted_list
